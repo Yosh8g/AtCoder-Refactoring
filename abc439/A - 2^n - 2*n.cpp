84d2a777b73fd024b242036ac0752cfbcc10563b
Yosh8g <yosh8g@gmail.com>
2026-02-01 00:50:17 +0900

    Create A - 2^n - 2*n.cpp

---
+++ abc439/A - 2^n - 2*n.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define endl "\n"
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    int pow = 1;
+    rep(i, n) {
+        pow *= 2;
+    }
+    cout << pow - 2 * n << endl;
+}
+
+// static_cast<int>(pow(2, n) - 2 * n) << endl; だけでもいけるっぽい（cppの累乗めんどいな）
